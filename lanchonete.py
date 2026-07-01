623a2a407fffa24570198c800f3220d48592baaa
rosimarmara2022-lang <rosimarmara2022@gmail.com>
2026-06-30 22:13:49 -0300

    Add files via upload

---
+++ lanchonete.py
@@ -0,0 +1,30 @@
+print ("=== Lá no Charuri ===" )
+print(" " * 4, "Lanchonete" )
+
+nome_cliente = input("Por favor, digite o seu nome ")
+print(f"Olá, {nome_cliente}")
+
+print("=== NOSSO CARDÁPIO === ")
+print("1. Hamburguer - R$ 39.59")
+print("1. Hamburguer  - R$ 39.59")
+print("1. Hamburguer  - R$ 39.59")
+
+# Recebendo dados do pedido
+print("\n Faça seu pedido")
+qtd_hamburguer = int (input("Quantos hamburgueres você deseja? "))
+qtd_hamburguer = int (input("Quantos hamburgueres você deseja? "))
+qtd_hamburguer = int (input("Quantos hamburgueres você deseja? "))
+# Cardápio finalizado
+# Fechando a conta
+total_hamburguer = qtd_hamburguer * 39.59 
+# Exibindo o cupim fiscal
+print("\n" + "="*30)
+print(" " * 8 + "CUPOM FISCAL" + " " * 8)
+print("=" * 30)
+print(f"Cliente: {nome_cliente} ")
+print(f" Hamburguer: {total_hamburguer}")
+print(f"Total do pedido: {total_hamburguer}")
+
+
+
+
